a53c10eb4dbc6a54083f92efd5dcd10d2ec8aa0a
colocky <callmeadam.ar@gmail.com>
2026-04-09 13:52:04 -0500

    Completed step 3.
    1. Create variables for an item name (String) and item price (double)
    2. Create a quantity variable (int)
    3. Build and print: EX. "You bought 3 apples for $7.10"

--- src/main/java/com/pluralsight/Reciept.java
+++ src/main/java/com/pluralsight/Reciept.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
 public class Reciept {
+    public static void main(String[] args) {
+
+        String product = "Dinosaur toy/s";
+        double productPrice = 3.99;
+        int productAmount = 2;
+
+        System.out.println("You bought " + productAmount + " " + product + " for $" + (productAmount * productPrice) + "!");
+    }
 }
